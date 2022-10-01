5bbcefaded5352f0d72d5b303f01c9e9e58d91a4
ishant <ishantkumar1412@gmail.com>
2022-10-01 10:26:06 +0530

    added_new_leetcode_problem

---
+++ leetCode Solutions/Q94_BinaryTreeInorderTraversal/BinaryTreeInorderTraversal.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static void inorder(TreeNode root,List<Integer> al)
+    {
+        // proceed if root is not null
+        if(root!=null)
+        {
+            // Go to left side
+            inorder(root.left,al);
+            // insert root node
+            al.add(root.val);
+            // Go to right side
+            inorder(root.right,al);
+        }
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        // Make answer list that stores the result
+        List<Integer> ans=new ArrayList<>();
+        // function that find out inorder traversal of tree and store in ans List
+        inorder(root,ans);
+        return ans;
+    }
+}
