fe26c3d1332419c3f5fc932335eff8969710946d
Aanya Kumari <101557638+Aanya9693@users.noreply.github.com>
2022-10-01 18:16:09 +0530

    Added Solution of Q31 of Leetcode
    I have added the solution of Q31 (Next Permutation in CPP ) of leetcode.

---
+++ leetCode Solutions/Q31_Next Permutation.cpp
@@ -0,0 +1,27 @@
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& a) {
+        int idx=-1;
+  
+        int n = a.size();
+        for(int i=n-1; i>0; i--){
+            if(a[i]>a[i-1]){
+                idx=i;
+                break;
+            }
+        }
+        if(idx == -1){
+            reverse(a.begin(), a.end());
+        }else{
+            int prev = idx;
+            for(int i=idx+1; i<n; i++){
+                if(a[i]>a[idx-1] and a[i]<=a[prev]){
+                    prev=i;
+                }
+            }
+            swap(a[idx-1],a[prev]);
+            reverse(a.begin()+idx,a.end());
+        }
+    }
+};
