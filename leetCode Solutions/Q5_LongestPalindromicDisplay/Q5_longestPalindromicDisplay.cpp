64cce09ed63f38f4b32181981fb5310fa9ac8023
Niket Bahety <86421121+NiketBahety@users.noreply.github.com>
2022-10-01 11:18:01 +0530

    O(n^2) solution
    This is a 2-pointer approach and it passes all test cases and is accepted on Leetcode.

--- leetCode Solutions/Q5_LongestPalindromicDisplay/Q5_longestPalindromicDisplay.cpp
+++ leetCode Solutions/Q5_LongestPalindromicDisplay/Q5_longestPalindromicDisplay.cpp
@@ -1,27 +1,44 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
 class Solution {
 public:
     string longestPalindrome(string s) {
-
-		string revS = s;
-		vector<int> map(s.length());
-		for (int k = 0; k < s.length(); k++){
-
-		}
-			// reverse(revS.begin(), revS.end());
-
-		int i, j, front = 0, back = s.length();
-
-		i = 0;
-		j = s.length() - 1;
-
-		for (int k = i; k <= j; k++){
-			if(s[i] == s[j]){
-				
-			}
-		}
-	}
+        int n=s.length();
+        int max=0;
+        string maxstr="";
+        for(int i=0; i<n; i++){
+            int temp=0;
+            int lo=i,hi=i;
+            while(lo >= 0 && hi < n){
+                if(s[lo]==s[hi]){
+                    temp=(hi-lo+1);
+                }
+                else
+                    break;
+                if(temp > max){
+                    max=temp;
+                    maxstr=s.substr(lo,temp);
+                }
+                lo--;
+                hi++;
+            }
+            if(i!=n-1){
+                int l=i,h=i+1;
+                if(s[i]==s[i+1]){
+                    int t=0;
+                    while(l >= 0 && h < n){
+                        if(s[l]==s[h])
+                            t=(h-l+1);
+                        else
+                            break;
+                        if(t > max){
+                        max=t;
+                        maxstr=s.substr(l,t);
+                    }
+                        l--;
+                        h++;
+                    }
+                } 
+            }
+        }
+        return maxstr;
+    }
 };
